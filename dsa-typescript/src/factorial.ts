15422df15908b704ea70ff45c8ca72d8a18f6924
finalSay9 <chimwazaevan1@gmail.com>
2026-06-05 15:04:23 +0200

    successfully created the factorial algorithm using ts

--- dsa-typescript/src/factorial.ts
+++ dsa-typescript/src/factorial.ts
@@ -1,8 +1,10 @@
 
 function facto(num: number): number {
+    //if num is 0 or 1 give us 1 
     if(num ===0 || num === 1) {
         return 1
     }
+    //if its above 1 compute the factorial
     else {
        return num * facto(num - 1)
     }
