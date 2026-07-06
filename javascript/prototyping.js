bf222a00a344ab079fa6f2c3a61daf95660afba6
finalSay9 <chimwazaevan1@gmail.com>
2026-07-06 17:21:19 +0200

    learning js dynamic typing

---
+++ javascript/prototyping.js
@@ -0,0 +1,16 @@
+//dynamic typing and dynamic objects
+
+/**
+ * here wallet is
+ * starting as a number
+ */
+let wallet = 50;
+
+/**
+ * now changed to 
+ * string, thats dynamic 
+ * typing easily changed
+ * to string
+ */
+wallet = 'empty';
+console.log(wallet)
--- reasoning/sorting.py
+++ reasoning/sorting.py
@@ -2,3 +2,6 @@
 #  sort them
 
 numbers: list[int] = [2,5,0,4,1,9,3,11,7]
+
+for i in range(len(numbers)):
+    print(i)
