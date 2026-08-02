bfda7144a1d416073d5ce250bcc192a9fd31fc57
finalSay9 <chimwazaevan1@gmail.com>
2026-08-02 22:19:18 +0200

    the deep mind alena

--- dsa-typescript/src/arrays.ts
+++ dsa-typescript/src/arrays.ts
@@ -2,3 +2,17 @@
 const add = (a: number, b: number): number => {
     return a * b;
 }
+
+const User = {
+    name: string;
+    age: number;
+    email: string;
+    address: string;
+}
+
+const user: User = {
+    name: 'evan',
+    age: 23,
+    email: 'evan@gmail.com',
+    address: 'lilongwe'
+}
