38b739860e60f277150d88473b09e4ad1ea2fa9b
finalSay9 <chimwazaevan1@gmail.com>
2026-08-03 09:55:02 +0200

    the deep mind alena

--- dsa-typescript/src/arrays.js
+++ dsa-typescript/src/arrays.js
@@ -1,2 +1,22 @@
-var age = 30;
-console.log(age);
+var add = function (a, b) {
+    return a + b;
+};
+console.log(add(18, 18));
+var user = {
+    name: 'evan',
+    age: 23,
+    email: 'evan@gmail.com',
+    address: 'lilongwe'
+};
+console.log(user);
+;
+var details = {
+    name: 'evan',
+    discount: 12.3
+};
+console.log(details);
+var data = {
+    name: 'evan chimwaza',
+    address: 'aliko street'
+};
+console.log(data);
--- dsa-typescript/src/arrays.ts
+++ dsa-typescript/src/arrays.ts
@@ -1,44 +1,31 @@
-
-const add = (a: number, b: number): number => {
-    return a + b;
-}
-console.log(add(18,18))
-type User = {
-    name: string;
-    age: number;
-    email: string;
-    address: string;
+type Address = {
+    name: string,
+    id: number
 }
 
-const user: User = {
-    name: 'evan',
-    age: 23,
-    email: 'evan@gmail.com',
-    address: 'lilongwe'
+interface Company {
+    name: string,
+    address: Address,
+    no_of_employees: number,
+    departments: number,
+    registered: boolean
 }
 
-console.log(user)
-
-//inheritance interfaces
-
-interface client  {name: string};
-interface VipClient extends client {discount: number}
-
-const details: VipClient = {
-    name: 'evan',
-    discount: 12.3
+interface Person extends Company {
+    email: string,
+    age: number,
 }
 
-console.log(details)
-
-type member = {
-    name: string
+const info: Person = {
+    name: 'techvach',
+    address: {
+        name: 'welo street',
+        id: 1234
+    },
+    no_of_employees: 343,
+    departments: 34,
+    registered: true,
+    email: 'evanchimwaza@gmail.com',
+    age: 23
 }
-type VipMember = member & {address: string}
-
-const data: VipMember = {
-    name: 'evan chimwaza',
-    address: 'aliko street'
-}
-
-console.log(data)
+console.log(info)
